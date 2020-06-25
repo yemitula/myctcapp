049f38fba8715aebb0d2d2da3757e1edf5a71b1a
Yemi Tula <yemi@tulabyte.net>
2020-06-25 12:49:01 +0100

    thursday june 25 class
    - Angular components
    - Navigation
    - Routing
    - One way data binding
    - Two way data binding
    - ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+// imports
 import { Component } from '@angular/core';
 
+// decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// logic
 export class AppComponent {
   title = 'myctcapp';
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  name: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
+    console.log(this.clickCounter);
+  }
+
+}
